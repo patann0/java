59bed8f5f08dbb7d07259429c64407e9f9f9c42b
cororonn <35837967+cororonn@users.noreply.github.com>
2018-08-06 11:52:57 +0900

    Cabu.java

---
+++ Cabu.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class Cabu{
+      public static void main(String[] args)throws Exception {
+          for(int i = 0;i < 100;i++);
+          System.out.println(i);
+          
+          int k = 0;
+          while(k < 10){
+              System.out.println(k);
+              k++;
+          }
+          }
+    }
