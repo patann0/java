18f5be1a12f2c59f91b974029300debfdd6f237a
cororonn <35837967+cororonn@users.noreply.github.com>
2018-08-14 11:46:21 +0900

    A.java　に変更

--- A.java
+++ A.java
@@ -1,14 +1,18 @@
 import java.util.*;
 
-public class Cabu{
+public class A{
       public static void main(String[] args)throws Exception {
           for(int i = 0;i < 100;i++);
-          System.out.println(i);
+          System.out.println("aaaa");
           
           int k = 0;
           while(k < 10){
               System.out.println(k);
               k++;
           }
-          }
-    }
+          sub();
+      }
+      public static void sub(){
+          System.out.println("test commit");
+      }
+}
