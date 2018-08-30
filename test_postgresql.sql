482ae98a7ba404386c786955082a66afc551d335
cororonn <35837967+cororonn@users.noreply.github.com>
2018-08-30 11:17:33 +0900

    test_postgresql file new create!

---
+++ test_postgresql.sql
@@ -0,0 +1,8 @@
+create table aaa(id integer primary key,name text,foo varchar,bar timestamp);
+
+insert into aaa(id,name,foo,bar)values(1,'takada','MI','30 August 2018');
+insert into aaa(id,name,foo,bar)values(2,'uduki','LU','30 August 2018');
+insert into aaa(id,name,foo,bar)values(3,'iidabashi','TY','30 Aygust 2018');
+
+
+select * from aaa;
